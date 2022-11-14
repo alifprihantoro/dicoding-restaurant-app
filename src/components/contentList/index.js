1b959f6e862ecbcd9fb0f5b19c671a666dfa70f0
alifprihantoro <alifmuryp@gmail.com>
2022-11-14 15:55:05 +0700

    fix: delete not use now

--- src/components/contentList/index.js
+++ src/components/contentList/index.js
@@ -12,7 +12,7 @@ class ContentList extends HTMLElement {
       <h2 class="title">list restaurants</h2>
       <div class="content">
         ${restaurants
-          .map((e, i) => {
+          .map((e) => {
             return `
               <div class="card">
                 <div class="cover-image">
@@ -20,10 +20,7 @@ class ContentList extends HTMLElement {
                     src="${e.pictureId}"
                     alt="${e.name}"
                   />
-                  <button 
-                    value="${i}"
-                    class="detail-show"
-                    >show detail</button>
+                  <button>show detail</button>
                   <h4 class="right">${iconStar()} ${e.rating}</h4>
                   <h4>${iconMap()} ${e.city}</h4>
                   <h3>${e.name}</h3>
@@ -35,15 +32,6 @@ class ContentList extends HTMLElement {
           .join('')}
       </div>
     `
-    const imgEL = document.getElementsByClassName('detail-show')
-    for (let i = 0; i < imgEL.length; i++) {
-      const imgKlick = imgEL[i]
-      imgKlick.onclick = function (e) {
-        const INDEX_DATA = e.target.value
-        const { name } = restaurants[INDEX_DATA]
-        
-      }
-    }
   }
 }
 customElements.define('content-list', ContentList)
