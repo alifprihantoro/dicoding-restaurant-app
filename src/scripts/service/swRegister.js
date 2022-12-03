24f249a5c50a694c865f985d08f61f81b9e966c9
alifprihantoro <alifmuryp@gmail.com>
2022-12-03 14:18:27 +0700

    fixed: pwa,btn like

--- src/scripts/service/swRegister.js
+++ src/scripts/service/swRegister.js
@@ -1,13 +1,10 @@
 const swRegister = async () => {
   if (!('serviceWorker' in navigator)) {
-    console.log('Browser tidak mendukung Service Worker')
+    alert('browser tidak mendukung offline mode!')
     return
   }
   try {
     await navigator.serviceWorker.register('./sw.js')
-//     navigator.serviceWorker.ready.then(swRegistration => {
-//   return swRegistration.sync.register('foo');
-// });
   } catch (error) {
     console.log('Failed to register service worker', error)
   }
