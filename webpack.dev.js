5489e4d641d77ddec1e6ab58626270fe1f671102
alifprihantoro <alifmuryp@gmail.com>
2022-12-07 17:32:32 +0700

    fixed: add server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,6 @@
-const path = require('path')
 const { merge } = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const common = require('./webpack.common')
-
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
