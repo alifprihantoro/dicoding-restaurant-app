7e1dc11e6020091d63f21872c787a6f43d96767b
alifprihantoro <alifmuryp@gmail.com>
2023-01-03 19:47:27 +0700

    feat: add lazy load, image optimize

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,33 @@
-// component
-import renderHome from './pages/home'
-
-// service
-import swRegister from './service/swRegister'
-
-window.addEventListener('load', () => {
-  renderHome()
-  swRegister()
-})
+// reset style
+import './var.css'
+import './reset.css'
+import './mobile.css'
+// home page start ---
+import './pages/home/title.css'
+import './pages/home/warning.css'
+// nav
+import './pages/home/components/nav/main.css'
+import './pages/home/components/nav/mobile.css'
+// hero el
+import './pages/home/components/heroEl/main.css'
+import './pages/home/components/heroEl/mobile.css'
+// footer
+import './pages/home/components/footer/main.css'
+import './pages/home/components/footer/mobile.css'
+// content list
+import './pages/home/components/contentList/main.css'
+import './pages/home/components/contentList/mobile.css'
+// end home --
+// detail start --
+import './pages/detail/main.css'
+import './pages/detail/mobile.css'
+// bottom
+import './pages/detail/bottom/main.css'
+// review
+import './pages/detail/bottom/review/main.css'
+// menus
+import './pages/detail/menus/main.css'
+import './pages/detail/menus/desktop.css'
+// short info
+import './pages/detail/shortInfo/main.css'
+import './pages/detail/shortInfo/mobile.css'
